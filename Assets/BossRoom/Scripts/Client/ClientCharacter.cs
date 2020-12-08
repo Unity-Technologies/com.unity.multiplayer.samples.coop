f9a93cbc3d8bd4b5ced4627d30c03b33468ad53d
Luke Stampfli <luke@stampfli.one>
2020-12-08 16:10:59 +0000

    Character spawning and simple input handling and positon replication

---
+++ Assets/BossRoom/Scripts/Client/ClientCharacter.cs
@@ -0,0 +1,33 @@
+using System;
+using MLAPI;
+using UnityEngine;
+
+[RequireComponent(typeof(NetworkCharacterState))]
+public class ClientCharacter: NetworkedBehaviour
+{
+    private NetworkCharacterState networkCharacterState;
+
+    [SerializeField]
+    private Transform clientInterpolatedObject;
+
+    public override void NetworkStart()
+    {
+        if (!IsClient)
+        {
+            enabled = false;
+        }
+    }
+
+    void Awake()
+    {
+        networkCharacterState = GetComponent<NetworkCharacterState>();
+    }
+
+    void Update()
+    {
+        // TODO Needs core sdk support. This and rotation should grab the interpolated value of network position based on the last received snapshots.
+        clientInterpolatedObject.position = networkCharacterState.NetworkPosition.Value;
+
+        clientInterpolatedObject.rotation = Quaternion.Euler(0, networkCharacterState.NetworkRotationY.Value, 0);
+    }
+}
---
+++ Assets/BossRoom/Scripts/Shared/NetworkCharacterState.cs
@@ -0,0 +1,22 @@
+using System;
+using MLAPI.Messaging;
+using MLAPI.NetworkedVar;
+using MLAPI;
+using UnityEngine;
+
+// RPCStateComponent from the GDD
+public class NetworkCharacterState : NetworkedBehaviour
+{
+    public NetworkedVarVector3 NetworkPosition;
+    public NetworkedVarFloat NetworkRotationY;
+
+    // TODO Should we use Unity events or c# events?
+    public event Action<Vector3> OnReceivedClientInput;
+
+    [ServerRPC]
+    public void ServerRpcReceiveMovementInput(Vector3 position)
+    {
+        // Assumption that RPC is snaphshotted and buffered already here
+        OnReceivedClientInput?.Invoke(position);
+    }
+}
