d59a9b5ca284b5cbdbd2e868d1f0fde074d560ae
David Woodruff <david@stormflaggames.com>
2020-12-10 14:52:50 -0500

    fixing issue causing blank IPs to not work

--- Assets/Scripts/Client/UI/MainMenuUI.cs
+++ Assets/Scripts/Client/UI/MainMenuUI.cs
@@ -12,15 +12,10 @@ namespace BossRoomViz
         public GameObject GameHubGO;
         private BossRoom.GameNetHub m_netHub;
 
-        private string m_defaultIPText;
-
         // Start is called before the first frame update
         void Start()
         {
             m_netHub = GameHubGO.GetComponent<BossRoom.GameNetHub>();
-
-            //track what this is on startup. We're not opinionated; we will try whatever the user puts in, as long as they've changed the text.
-            m_defaultIPText = GameObject.Find("IPInputBox").GetComponent<UnityEngine.UI.Text>().text;
         }
 
         // Update is called once per frame
@@ -36,7 +31,7 @@ namespace BossRoomViz
         private string GetIPAddress()
         {
             string iptext = GameObject.Find("IPInputText").GetComponent<UnityEngine.UI.Text>().text;
-            if( iptext == m_defaultIPText )
+            if( iptext == "" )
             {
                 return "127.0.0.1";
             }
