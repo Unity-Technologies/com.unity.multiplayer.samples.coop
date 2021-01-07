1ef6b80fcc3dc8639e4cc8651e9de4ab4af94597
Fernando Cortez <fernando.cortez@unity3d.com>
2021-01-07 16:04:45 -0500

    spawner: added collisions for damage, clientrpc calls for spawning, inherits networkbehaviour now

--- Assets/BossRoom/Scripts/Server/ServerWaveSpawner.cs
+++ Assets/BossRoom/Scripts/Server/ServerWaveSpawner.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using BossRoom.Shared;
 using UnityEngine;
 using MLAPI;
 using MLAPI.Messaging;
+using MLAPI.NetworkedVar;
 
 namespace BossRoom.Server
 {
     /// <summary>
     /// Component responsible for spawning prefab clones in waves on the server.
     /// </summary>
-    public class ServerWaveSpawner : MonoBehaviour
+    public class ServerWaveSpawner : NetworkedBehaviour
     {
+        // amount of hits it takes to break any spawner
+        const int k_maxHealth = 3;
+        
+        // current health of the spawner
+        public NetworkedVarInt health;
+        
         // proposal: reference a RuntimeList of players in game (list for now)
         [SerializeField]
         private List<NetworkedObject> m_Players;
@@ -23,7 +31,7 @@ namespace BossRoom.Server
         private Transform m_Transform;
         
         // track wave index and reset once all waves are complete
-        private int m_WaveIndex = 0;
+        private int m_WaveIndex;
         
         // keep reference to our wave spawning coroutine
         private Coroutine m_WaveSpawning;
@@ -53,39 +61,41 @@ namespace BossRoom.Server
         [Tooltip("Once last wave is spawned, the spawner waits this long to restart wave spawns, in seconds.")]
         public float restartDelay;
         [Tooltip("A player must be withing this distance to commence first wave spawn.")]
-        public float proximityDistance = 50f;
+        public float proximityDistance;
         [Tooltip("After being broken, the spawner waits this long to restart wave spawns, in seconds.")]
-        public float dormantCooldown = 60f;
-
-        // TODO [GOMPS-81] Current workaround for not inheriting from NetworkedBehaviour for NetworkStart event
-        private bool m_ServerStartedCallbackSet;
+        public float dormantCooldown;
 
         void Awake()
         {
             m_Transform = transform;
         }
-
-        // TODO [GOMPS-81] Invoke StartPlayerProximityValidation on proper NetworkStart callback 
-        void Update()
+        
+        public override void NetworkStart()
         {
-            if (NetworkingManager.Singleton == null || m_ServerStartedCallbackSet)
+            base.NetworkStart();
+            
+            if (!IsServer)
             {
-                return;
+                enabled = false;
             }
+            
+            // hack for now
+            m_Players.Add(GameObject.FindObjectOfType<NetworkCharacterState>().GetComponent<NetworkedObject>());
 
-            NetworkingManager.Singleton.OnServerStarted += StartPlayerProximityValidation;
-            m_ServerStartedCallbackSet = true;
+            health.Value = k_maxHealth;
             m_Hit = new RaycastHit[1];
+            StartPlayerProximityValidation();
         }
         
         void StartPlayerProximityValidation()
         {
+            m_WaveSpawning = null;
             StopAllCoroutines();
             StartCoroutine(ValidatePlayersProximity(StartWaveSpawning));
         }
 
         /// <summary>
-        /// 
+        /// Coroutine for continually validating proximity to players and invoking an action when any is near.
         /// </summary>
         /// <param name="validationAction"></param>
         /// <returns></returns>
@@ -150,26 +160,31 @@ namespace BossRoom.Server
         {
             for (var i = 0; i < spawnsPerWave; i++)
             {
-                SpawnPrefabServerRpc();
+                SpawnPrefabClientRpc();
 
                 yield return new WaitForSeconds(timeBetweenSpawns);
             }
 
             m_WaveIndex++;
         }
-
+        
+        // NOTE: This is not being fired on the Host (the server-client)
         /// <summary>
         /// Server Rpc to spawn a NetworkedObject prefab clone.
         /// </summary>
-        [ServerRpc]
-        void SpawnPrefabServerRpc()
+        [ClientRpc]
+        void SpawnPrefabClientRpc()
         {
             if (m_NetworkedPrefab == null)
             {
                 return;
             }
-            
-            var clone =  Instantiate(m_NetworkedPrefab, m_Transform.position, Quaternion.identity);
+
+            var spawnPosition = m_Transform.position + 
+                                new Vector3(UnityEngine.Random.Range(-1f, 1f), 
+                                    UnityEngine.Random.Range(-1f, 1f), 
+                                    UnityEngine.Random.Range(-1f, 1f));
+            var clone =  Instantiate(m_NetworkedPrefab, spawnPosition, Quaternion.identity);
             if (!clone.IsSpawned)
             {
                 clone.Spawn();
@@ -189,6 +204,8 @@ namespace BossRoom.Server
 
             var spawnerPosition = m_Transform.position;
 
+            // iterate through players and only return true if a player is in range
+            // and is not blocked by a blocking collider.
             foreach (var player in m_Players)
             {
                 var playerPosition = player.transform.position;
@@ -202,6 +219,7 @@ namespace BossRoom.Server
                 var direction = playerPosition - spawnerPosition;
                 m_Ray = new Ray(spawnerPosition, direction);
 
+                // TODO: sort out layer for terrain/walls/blocker that this ray will try to hit
                 var hit = Physics.RaycastNonAlloc(m_Ray, m_Hit, 
                     Mathf.Min(direction.magnitude, proximityDistance),m_BlockingMask);
                 if (hit == 0)
@@ -216,7 +234,6 @@ namespace BossRoom.Server
         /// <summary>
         /// Immediately stops current wave spawns and restarts proximity check after cooldown. 
         /// </summary>
-        [ContextMenu("Cooldown")]
         void StartWaveSpawnCooldown()
         {
             StopAllCoroutines();
@@ -230,8 +247,66 @@ namespace BossRoom.Server
         IEnumerator WaveSpawnCooldown()
         {
             yield return new WaitForSeconds(dormantCooldown);
-            
+
+            // revive spawner back up to full health and visually show a revival
+            ReviveClientRpc();
+            health.Value = k_maxHealth;
             StartPlayerProximityValidation();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!IsServer)
+            {
+                return;
+            }
+            
+            // TODO: sort out layer for player weapon that this collider will listen for (through Physics matrix)
+            if (!other.gameObject.CompareTag(("Weapon")))
+            {
+                return;
+            }
+
+            health.Value--;
+
+            if (health.Value <= 0)
+            {
+                BrokenClientRpc();
+                
+                // this spawner is dead; commence a cooldown coroutine before being revived
+                StartWaveSpawnCooldown();
+            }
+            else
+            {
+                ReceiveHitClientRpc();
+            }
+        }
+
+        /// <summary>
+        /// RPC sent from the server to display on client side that this spawner has been damaged.
+        /// </summary>
+        [ClientRpc]
+        void ReceiveHitClientRpc()
+        {
+            // TODO: fire hit animation here
+        }
+        
+        /// <summary>
+        /// RPC sent from the server to display on client side that this spawner has been broken.
+        /// </summary>
+        [ClientRpc]
+        void BrokenClientRpc()
+        {
+            // TODO: fire die animation here
+        }
+        
+        /// <summary>
+        /// RPC sent from the server to display on client side that this spawner has been revived.
+        /// </summary>
+        [ClientRpc]
+        void ReviveClientRpc()
+        {
+            // TODO: fire revive animation here
         }
     }
 }
