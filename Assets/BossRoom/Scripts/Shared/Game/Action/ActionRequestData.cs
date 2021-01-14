00da8d48ea827f8ad340c13a307394d03e229a18
David Woodruff <david@stormflaggames.com>
2021-01-14 15:35:20 -0500

    Developer Notes:
        -- added HitReact state to CharacterSetController (triggered by "BeginHitReact")
        -- Made everybody have a ClientCharacter component.
        -- Added a ClientCharacter->ClientCharacterVisualization reference, so that you can
           get the visual GameObject from a NetworkId.
        -- Fixed issue where placed Boss wasn't on the right Layer, making him invincible.
        -- Hooked up some very preliminary HitReact logic in ClientCharacterVisualization (will be refactored shortly as part of ActionVisualizer).
        -- ChaseAction now rotates you to target even if you don't move.
        -- ChaseAction no longer "inches forward" every time you attack.
        -- MeleeAction now does a preliminary DetectFoe in Start. There's a lengthy comment explaining this, for educational purposes.
        -- The TANK_BASEATTACK range has been tuned to look better.
        -- ActionRequestData now checks which fields are "non-default" and uses that to populate a flags byte, rather than relying on static data to determine which fields should serialize. This is better, because sometimes fields are just optional (true for the MeleeAction). I thought you could serialize "null" for an array, but that doesn't actually work.
        -- got new .editorconfig. Applied changes to the files I modified.
    
    Squashed commit of the following:
    
    commit 1895d050afef3a7873fa33447cfae4b54d6b2687
    Merge: 0ceea9e 9a69099
    Author: David Woodruff <david@stormflaggames.com>
    Date:   Thu Jan 14 15:34:33 2021 -0500
    
        taking develop merge
    
    commit 0ceea9e7c5b7b7884116f058823a489112e17202
    Author: David Woodruff <david@stormflaggames.com>
    Date:   Thu Jan 14 15:31:42 2021 -0500
    
        Developer Notes:
    
        -- added HitReact state to CharacterSetController (triggered by "BeginHitReact")
        -- Made everybody have a ClientCharacter component.
        -- Added a ClientCharacter->ClientCharacterVisualization reference, so that you can
           get the visual GameObject from a NetworkId.
        -- Fixed issue where placed Boss wasn't on the right Layer, making him invincible.
        -- Hooked up some very preliminary HitReact logic in ClientCharacterVisualization (will be refactored shortly as part of ActionVisualizer).
        -- ChaseAction now rotates you to target even if you don't move.
        -- ChaseAction no longer "inches forward" every time you attack.
        -- MeleeAction now does a preliminary DetectFoe in Start. There's a lengthy comment explaining this, for educational purposes.
        -- The TANK_BASEATTACK range has been tuned to look better.
        -- ActionRequestData now checks which fields are "non-default" and uses that to populate a flags byte, rather than relying on static data to determine which fields should serialize. This is better, because sometimes fields are just optional (true for the MeleeAction). I thought you could serialize "null" for an array, but that doesn't actually work.
        -- got new .editorconfig. Applied changes to the files I modified.
    
        Squash Summary:
    
        Squashed commit of the following:
    
        commit a3b984120f72d4ce3aeb60fb3b193639229f7187
        Author: David Woodruff <david@stormflaggames.com>
        Date:   Thu Jan 14 15:30:12 2021 -0500
    
            reformatting pass
    
        commit b51eb51717641a5b8a029a4f457e8a96cd166020
        Author: David Woodruff <david@stormflaggames.com>
        Date:   Thu Jan 14 15:11:02 2021 -0500
    
            deleting some debug logs
    
        commit cf4bd8b9b39194728299dacf7c61e13d19912fa6
        Author: David Woodruff <david@stormflaggames.com>
        Date:   Thu Jan 14 14:45:51 2021 -0500
    
            got animation blending better; fixed issue where boss was invisible. Redid ActionRequestData to dynamically decide which fields to serialize, by passing a flags byte.
    
        commit 5c13f00e11af651b9db970e8ece8b3d8fcf20f57
        Author: David Woodruff <david@stormflaggames.com>
        Date:   Thu Jan 14 11:43:15 2021 -0500
    
            working on melee improvements

--- Assets/BossRoom/Scripts/Shared/Game/Action/ActionRequestData.cs
+++ Assets/BossRoom/Scripts/Shared/Game/Action/ActionRequestData.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -77,37 +76,18 @@ namespace BossRoom
     }
 
     /// <summary>
-    /// metadata about each kind of ActionLogic. This basically just informs us what fields to serialize for each kind of ActionLogic. 
-    /// </summary>
-    public class ActionLogicInfo
-    {
-        public bool HasPosition;
-        public bool HasDirection;
-        public bool HasTarget;
-        public bool HasAmount;
-    }
-
-    /// <summary>
     /// FIXME [GOMPS-99]: this list will be turned into a collection of Scriptable Objects. 
     /// Question: Do we want to show how to do skill levels, as I am doing here?
     /// </summary>
     public class ActionData
     {
-        public static Dictionary<ActionLogic, ActionLogicInfo> LogicInfos = new Dictionary<ActionLogic, ActionLogicInfo>
-        {
-            {ActionLogic.MELEE, new ActionLogicInfo{} },
-            {ActionLogic.RANGED, new ActionLogicInfo{HasDirection=true} },
-            {ActionLogic.RANGEDTARGETED, new ActionLogicInfo{HasTarget=true} },
-            {ActionLogic.CHASE, new ActionLogicInfo{HasTarget=true, HasAmount=true} },
-        };
-
         public static Dictionary<ActionType, List<ActionDescription>> ActionDescriptions = new Dictionary<ActionType, List<ActionDescription>>
         {
             { ActionType.TANK_BASEATTACK , new List<ActionDescription>
                 {
-                    {new ActionDescription{Logic=ActionLogic.MELEE, Amount=10, ManaCost=2, ExecTime_s=0.3f, Duration_s=0.5f, Range=4f, Anim="Todo" } },  //level 1
-                    {new ActionDescription{Logic=ActionLogic.MELEE, Amount=15, ManaCost=2, ExecTime_s=0.3f, Duration_s=0.5f, Range=4f, Anim="Todo" } },  //level 2
-                    {new ActionDescription{Logic=ActionLogic.MELEE, Amount=20, ManaCost=2, ExecTime_s=0.3f, Duration_s=0.5f, Range=4f, Anim="Todo" } },  //level 3
+                    {new ActionDescription{Logic=ActionLogic.MELEE, Amount=10, ManaCost=2, ExecTime_s=0.3f, Duration_s=0.5f, Range=2f, Anim="Todo" } },  //level 1
+                    {new ActionDescription{Logic=ActionLogic.MELEE, Amount=15, ManaCost=2, ExecTime_s=0.3f, Duration_s=0.5f, Range=2f, Anim="Todo" } },  //level 2
+                    {new ActionDescription{Logic=ActionLogic.MELEE, Amount=20, ManaCost=2, ExecTime_s=0.3f, Duration_s=0.5f, Range=2f, Anim="Todo" } },  //level 3
                 }
             },
 
@@ -119,10 +99,10 @@ namespace BossRoom
                 }
             },
 
-            { ActionType.GENERAL_CHASE, new List<ActionDescription> 
+            { ActionType.GENERAL_CHASE, new List<ActionDescription>
                 {
                     {new ActionDescription{Logic=ActionLogic.CHASE } }
-                } 
+                }
             }
         };
     }
@@ -146,29 +126,58 @@ namespace BossRoom
 
         //O__O Hey, are you adding something? Be sure to update ActionLogicInfo, as well as the methods below. 
 
+        //[System.Flags]
+        private enum PackFlags
+        {
+            None = 0,
+            HasPosition = 1,
+            HasDirection = 1 << 1,
+            HasTargetIds = 1 << 2,
+            HasLevel = 1 << 3,
+            HasAmount = 1 << 4,
+            ShouldQueue = 1 << 5
+            //currently serialized with a byte. Change Read/Write if you add more than 8 fields. 
+        }
+
+        private PackFlags GetPackFlags()
+        {
+            PackFlags flags = PackFlags.None;
+            if (Position != Vector3.zero) { flags |= PackFlags.HasPosition; }
+            if (Direction != Vector3.zero) { flags |= PackFlags.HasDirection; }
+            if (TargetIds != null) { flags |= PackFlags.HasTargetIds; }
+            if (Level != 0) { flags |= PackFlags.HasLevel; }
+            if (Amount != 0) { flags |= PackFlags.HasAmount; }
+            if (ShouldQueue) { flags |= PackFlags.ShouldQueue; }
+
+            return flags;
+        }
+
         public void Read(Stream stream)
         {
             using (var reader = MLAPI.Serialization.Pooled.PooledBitReader.Get(stream))
             {
                 ActionTypeEnum = (ActionType)reader.ReadInt16();
-                ShouldQueue = reader.ReadBool();
+                PackFlags flags = (PackFlags)reader.ReadByte();
 
-                var Logic = ActionData.ActionDescriptions[ActionTypeEnum][0].Logic;
-                var Info = ActionData.LogicInfos[Logic];
+                ShouldQueue = (flags & PackFlags.ShouldQueue) != 0;
 
-                if (Info.HasPosition)
+                if ((flags & PackFlags.HasPosition) != 0)
                 {
                     Position = reader.ReadVector3();
                 }
-                if (Info.HasDirection)
+                if ((flags & PackFlags.HasDirection) != 0)
                 {
                     Direction = reader.ReadVector3();
                 }
-                if (Info.HasTarget)
+                if ((flags & PackFlags.HasTargetIds) != 0)
                 {
                     TargetIds = reader.ReadULongArray();
                 }
-                if (Info.HasAmount)
+                if ((flags & PackFlags.HasLevel) != 0)
+                {
+                    Level = reader.ReadInt32();
+                }
+                if ((flags & PackFlags.HasAmount) != 0)
                 {
                     Amount = reader.ReadSingle();
                 }
@@ -179,24 +188,28 @@ namespace BossRoom
         {
             using (var writer = MLAPI.Serialization.Pooled.PooledBitWriter.Get(stream))
             {
-                ActionLogic Logic = ActionData.ActionDescriptions[ActionTypeEnum][0].Logic;
-                ActionLogicInfo Info = ActionData.LogicInfos[Logic];
+                PackFlags flags = GetPackFlags();
 
                 writer.WriteInt16((short)ActionTypeEnum);
-                writer.WriteBool(ShouldQueue);
-                if (Info.HasPosition)
+                writer.WriteByte((byte)flags);
+
+                if ((flags & PackFlags.HasPosition) != 0)
                 {
                     writer.WriteVector3(Position);
                 }
-                if (Info.HasDirection)
+                if ((flags & PackFlags.HasDirection) != 0)
                 {
                     writer.WriteVector3(Direction);
                 }
-                if (Info.HasTarget)
+                if ((flags & PackFlags.HasTargetIds) != 0)
                 {
                     writer.WriteULongArray(TargetIds);
                 }
-                if (Info.HasAmount)
+                if ((flags & PackFlags.HasLevel) != 0)
+                {
+                    writer.WriteInt32(Level);
+                }
+                if ((flags & PackFlags.HasAmount) != 0)
                 {
                     writer.WriteSingle(Amount);
                 }
