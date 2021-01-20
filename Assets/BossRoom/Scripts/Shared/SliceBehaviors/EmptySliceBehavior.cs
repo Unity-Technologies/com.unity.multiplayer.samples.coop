1832ca8574d1f60e221d525edab0d5716cfb642b
eheimburg <74330250+eheimburg@users.noreply.github.com>
2021-01-20 04:10:05 -0500

    Update EmptySliceBehavior.cs
    Putting this back the way I found it (commented it out because I can't get standalone builds to run with it enabled, local issue)

--- Assets/BossRoom/Scripts/Shared/SliceBehaviors/EmptySliceBehavior.cs
+++ Assets/BossRoom/Scripts/Shared/SliceBehaviors/EmptySliceBehavior.cs
@@ -1,18 +1,16 @@
-/*
 using Unity.MultiPie;
 
 namespace BossRoom.Scripts.Shared.SliceBehaviors
 {
-public class EmptySliceBehavior : SliceBehavior
-{
-    public override void OnAllSlicesStarted()
+    public class EmptySliceBehavior : SliceBehavior
     {
-    }
+        public override void OnAllSlicesStarted()
+        {
+        }
 
-    public override void OnSliceMessageReceived(int senderId, string message)
-    {
-    }
+        public override void OnSliceMessageReceived(int senderId, string message)
+        {
+        }
 
+    }
 }
-}
-*/
