c5fa37ca2a33d1027d12b077a8c6992bc9d9ef77
eheimburg <74330250+eheimburg@users.noreply.github.com>
2021-02-03 22:20:10 -0500

    more comments about perf

--- Assets/BossRoom/Scripts/Shared/Game/TimedSelfDestruct.cs
+++ Assets/BossRoom/Scripts/Shared/Game/TimedSelfDestruct.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 /// <summary>
 /// Used by simple visual FX to self-destruct.
 /// </summary>
+/// <remarks>
+/// Note on mobile performance: on mobile, it's a major perf-hit to constantly create and
+/// destroy GameObjects; in mobile games you should use object pooling for your visual FX instead!
+/// </remarks>
 namespace BossRoom
 {
     public class TimedSelfDestruct : MonoBehaviour
