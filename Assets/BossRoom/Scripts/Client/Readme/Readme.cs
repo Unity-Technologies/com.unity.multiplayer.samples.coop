a78d92409515f900db398b15c622233e69b02219
Fernando Cortez <fernando.cortez@unity3d.com>
2021-02-17 13:14:46 -0500

    urp template readme imported for Boss Room

---
+++ Assets/BossRoom/Scripts/Client/Readme/Readme.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace BossRoom
+{
+    public class Readme : ScriptableObject
+    {
+        public Texture2D icon;
+        public string title;
+        public Section[] sections;
+        public bool loadedLayout;
+
+        [Serializable]
+        public class Section
+        {
+            public string heading;
+            public string text;
+            public string linkText;
+            public string url;
+        }
+    }
+}
