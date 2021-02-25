f32d5b6d435a8d1b336e36fbe618f0e2670dba9d
Philipp Deschain <philippd@unity3d.com>
2021-02-25 18:53:55 -0500

    NetworkCharacterTypeState needed to inherit from NetowrkedBehavior in order for it's NetVarto be synced.

--- Assets/BossRoom/Scripts/Shared/NetworkCharacterTypeState.cs
+++ Assets/BossRoom/Scripts/Shared/NetworkCharacterTypeState.cs
@@ -1,13 +1,14 @@
 using System;
+using MLAPI;
 using MLAPI.NetworkedVar;
 using UnityEngine;
 
 namespace BossRoom
 {
     /// <summary>
-    /// MonoBehaviour containing only one NetworkedVar which represents this character's health.
+    /// NetworkedBehaviour containing only one NetworkedVar which represents this character's health.
     /// </summary>
-    public class NetworkCharacterTypeState : MonoBehaviour
+    public class NetworkCharacterTypeState : NetworkedBehaviour
     {
         [Tooltip("NPCs should set this value in their prefab. For players, this value is set at runtime.")]
         public NetworkedVar<CharacterTypeEnum> CharacterType = new NetworkedVar<CharacterTypeEnum>();
