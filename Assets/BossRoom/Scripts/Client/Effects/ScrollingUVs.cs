abebfdc99fb6d0303d71b75a79b090d8f697b9a8
jdepuy <42752908+jdepuy@users.noreply.github.com>
2021-03-01 10:40:38 -0500

    interim art checkin for removing meta data from pngs, and scrolling lava

---
+++ Assets/BossRoom/Scripts/Client/Effects/ScrollingUVs.cs
@@ -0,0 +1,16 @@
+ using UnityEngine;
+ using System.Collections;
+ 
+ public class ScrollingUVs : MonoBehaviour 
+ {
+     public float ScrollX =.01f;
+     public float ScrollY =.01f;
+
+    void Update () 
+    {
+        float OffsetX = Time.time * ScrollX;
+        float OffsetY = Time.time * ScrollY;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+    }
+
+ }
