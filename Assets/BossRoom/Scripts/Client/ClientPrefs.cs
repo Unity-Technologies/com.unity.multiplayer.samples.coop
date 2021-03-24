2c371b0b85bb50ebf5b55b74edbb74b2434475cb
eheimburg <74330250+eheimburg@users.noreply.github.com>
2021-03-23 21:52:25 -0400

    Reverting scenes, refactoring into a separate canvas
    Reverted the Startup scene, then added the new prefab to it. The other scenes should all be unchanged from pre-patch now. Hopefully. (git was giving strange error messages about BossRoom.unity, but in any case, there SHOULD be no changes in this PR to any scene except Startup)

--- Assets/BossRoom/Scripts/Client/ClientPrefs.cs
+++ Assets/BossRoom/Scripts/Client/ClientPrefs.cs
@@ -9,14 +9,27 @@ namespace BossRoom.Client
     /// </summary>
     public class ClientPrefs
     {
+        private const float k_DefaultMasterVolume = 1;
+        private const float k_DefaultMusicVolume = 0.8f;
+
         public static float GetMasterVolume()
         {
-            return PlayerPrefs.GetFloat("MasterVolume", 1);
+            return PlayerPrefs.GetFloat("MasterVolume", k_DefaultMasterVolume);
         }
 
         public static void SetMasterVolume(float volume)
         {
             PlayerPrefs.SetFloat("MasterVolume", volume);
+        }
+
+        public static float GetMusicVolume()
+        {
+            return PlayerPrefs.GetFloat("MusicVolume", k_DefaultMusicVolume);
+        }
+
+        public static void SetMusicVolume(float volume)
+        {
+            PlayerPrefs.SetFloat("MusicVolume", volume);
         }
 
         /// <summary>
