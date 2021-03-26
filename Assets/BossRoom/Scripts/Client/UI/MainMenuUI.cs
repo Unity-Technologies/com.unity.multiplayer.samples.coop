c549ab2ee1cc5d1a5f6333ce364c51e16d3e3096
Eric Heimburg <74330250+eheimburg@users.noreply.github.com>
2021-03-26 15:31:45 -0400

    [GOMPS-270] bossroom should let users specify connection port# (#205)

--- Assets/BossRoom/Scripts/Client/UI/MainMenuUI.cs
+++ Assets/BossRoom/Scripts/Client/UI/MainMenuUI.cs
@@ -38,7 +38,7 @@ namespace BossRoom.Visual
         public void OnHostClicked()
         {
             m_ResponsePopup.SetupEnterGameDisplay(true, "Host Game", "Input the IP to host on", "Input the room name to host on", "iphost", "Confirm",
-                (string connectInput, string playerName, OnlineMode onlineMode) =>
+                (string connectInput, int connectPort, string playerName, OnlineMode onlineMode) =>
             {
                 m_GameNetPortal.PlayerName = playerName;
                 switch (onlineMode)
@@ -48,16 +48,16 @@ namespace BossRoom.Visual
                         break;
 
                     case OnlineMode.IpHost:
-                        m_GameNetPortal.StartHost(PostProcessIpInput(connectInput), k_ConnectPort);
+                        m_GameNetPortal.StartHost(PostProcessIpInput(connectInput), connectPort);
                         break;
                 }
-            }, k_DefaultIP);
+            }, k_DefaultIP, k_ConnectPort);
         }
 
         public void OnConnectClicked()
         {
             m_ResponsePopup.SetupEnterGameDisplay(false, "Join Game", "Input the host IP below", "Input the room name below", "iphost", "Join",
-                (string connectInput, string playerName, OnlineMode onlineMode) =>
+                (string connectInput, int connectPort, string playerName, OnlineMode onlineMode) =>
             {
                 m_GameNetPortal.PlayerName = playerName;
 
@@ -68,11 +68,11 @@ namespace BossRoom.Visual
                         break;
 
                     case OnlineMode.IpHost:
-                        ClientGameNetPortal.StartClient(m_GameNetPortal, connectInput, k_ConnectPort);
+                        ClientGameNetPortal.StartClient(m_GameNetPortal, connectInput, connectPort);
                         break;
                 }
                 m_ResponsePopup.SetupNotifierDisplay("Connecting", "Attempting to Join...", true, false);
-            }, k_DefaultIP);
+            }, k_DefaultIP, k_ConnectPort);
         }
 
         private string PostProcessIpInput(string ipInput)
