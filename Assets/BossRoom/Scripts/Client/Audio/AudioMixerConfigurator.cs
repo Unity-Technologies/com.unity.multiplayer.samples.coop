a6b1e5683bf054e7acb9087a085d73e3fcbb677a
eheimburg <74330250+eheimburg@users.noreply.github.com>
2021-03-27 22:40:53 -0400

    Make sure configurator doesn't destroy on load
    (Not needed at the moment since its already on a dont-destroy-on-load game object, but just in case it gets moved somewhere else.)

--- Assets/BossRoom/Scripts/Client/Audio/AudioMixerConfigurator.cs
+++ Assets/BossRoom/Scripts/Client/Audio/AudioMixerConfigurator.cs
@@ -30,6 +30,7 @@ namespace BossRoom.Client
         private void Awake()
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
 
         private void Start()
