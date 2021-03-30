15492852d79c63ebc8236505d301b946f27269ed
Luke Stampfli <luke@stampfli.one>
2021-03-31 00:13:37 +0100

    docs: Add comment explaining why we need update to display room key

--- Assets/BossRoom/Scripts/Client/UI/RoomNameBox.cs
+++ Assets/BossRoom/Scripts/Client/UI/RoomNameBox.cs
@@ -32,6 +32,9 @@ public class RoomNameBox : MonoBehaviour
         }
     }
 
+    // This update loop exists because there is currently a bug in MLAPI which runs client connected callbacks before the transport has
+    // fully finished the asynchronous connection. That's why are loading the character select screen too early and need this update loop to
+    // update the room key once we are fully connected to the Photon cloud.
     void Update()
     {
         if (m_ConnectionFinished == false)
