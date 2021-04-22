a52f070ee35713f1006868228667d9b02e204685
David Woodruff <david@stormflaggames.com>
2021-04-22 09:38:56 -0400

    Bugfix/host quit strands some players (#288)
    * fixing issue where host quitting strands some clients in the game. Also fixed issue where no disconnect message was shown in MainMenu.
    
    * adding missed meta file
    
    * removing transitionreason in favor of a new DisconnectReason class that hangs off ClientGameNetPortal
    
    * moving disconnect reset to a better place. Now it will happen even if a client decides to become a host, and regardless of whether starting as a client or relay client.

--- Assets/BossRoom/Scripts/Client/Game/State/ClientPostGameState.cs
+++ Assets/BossRoom/Scripts/Client/Game/State/ClientPostGameState.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace BossRoom.Client
 {
     /// <summary>
@@ -7,6 +9,19 @@ namespace BossRoom.Client
     public class ClientPostGameState : GameStateBehaviour
     {
         public override GameState ActiveState { get { return GameState.PostGame; } }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            //it is common for the user to get dumped back to main menu from here (i.e., if the host decides not to play again), and
+            //it is a little funny to display a "Connection to Host Lost" message in that case. The best thing would probably be to
+            //display a "Host Abandoned the Game" message, but this would require some more plumbing (an RPC from the host before it quit,
+            //containing that information).
+            //In the meantime, we just set "UserRequested" to suppress the Disconnected error popup.
+            var portalGO = GameObject.FindGameObjectWithTag("GameNetPortal");
+            portalGO.GetComponent<ClientGameNetPortal>().DisconnectReason.SetDisconnectReason(DisconnectReasonType.UserRequested);
+        }
 
         public override void NetworkStart()
         {
