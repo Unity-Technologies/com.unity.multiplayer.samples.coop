3c9b587c0b82dfba572917a37803d5a911b7f83d
FPaizSF <fernando@stormflag.net>
2021-05-03 11:40:34 -0400

    Adjustments for balance and difficulty scaling with more players (#321)
    * added a new spawner portal in room before boss door. 
    
    * Lock in manually-changed scene differences so prefabs are canonical..
    
    * Change description of rogue basic attack as now it is slower but more powerful than hammer
    
    * balance tuning to action times and dmg.
    
    * tuned spawn distances for spawners in room 1 and boss room
    
    * adjusted spawn cap per player
    
    * removed some default imps and adjusted directions of placed imps
    
    * locking in changes to StaticNetworkObjects
    
    * added two null check safeguards for objects that get deleted during play
    
    
    Co-authored-by: jdepuy <42752908+jdepuy@users.noreply.github.com>
    Co-authored-by: eheimburg <74330250+eheimburg@users.noreply.github.com>
    Co-authored-by: David Woodruff <david@stormflaggames.com>

--- Assets/BossRoom/Scripts/Client/Game/Action/MeleeActionFX.cs
+++ Assets/BossRoom/Scripts/Client/Game/Action/MeleeActionFX.cs
@@ -112,7 +112,7 @@ namespace BossRoom.Visual
                         string hitAnim = Description.ReactAnim;
                         if(string.IsNullOrEmpty(hitAnim)) { hitAnim = k_DefaultHitReact; }
                         var clientChar = originalTarget.GetComponent<Client.ClientCharacter>();
-                        if (clientChar)
+                        if (clientChar && clientChar.ChildVizObject && clientChar.ChildVizObject.OurAnimator)
                         {
                             clientChar.ChildVizObject.OurAnimator.SetTrigger(hitAnim);
                         }
