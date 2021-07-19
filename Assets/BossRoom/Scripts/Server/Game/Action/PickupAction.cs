33d63c3634a6f06de64d212045c0d4de394be041
Fernando Cortez <fernando.cortez@unity3d.com>
2021-07-19 11:49:35 -0400

    layermask fix on the pickupaction script

--- Assets/BossRoom/Scripts/Server/Game/Action/PickupAction.cs
+++ Assets/BossRoom/Scripts/Server/Game/Action/PickupAction.cs
@@ -5,8 +5,6 @@ namespace BossRoom.Server
 {
     public class PickupAction : Action
     {
-        private bool m_ExecutionFired;
-
         RaycastHit[] m_RaycastHits = new RaycastHit[1];
 
         public PickupAction(ServerCharacter parent, ref ActionRequestData data) : base(parent, ref data)
@@ -15,10 +13,8 @@ namespace BossRoom.Server
 
         public override bool Start()
         {
-            ulong target = (Data.TargetIds != null && Data.TargetIds.Length > 0) ? Data.TargetIds[0] : m_Parent.NetState.TargetId.Value;
-
-            int numResults = Physics.BoxCastNonAlloc(m_Parent.transform.position, m_Parent.GetComponent<Collider>().bounds.extents,
-                m_Parent.transform.forward, m_RaycastHits, Quaternion.identity, Description.Range, LayerMask.NameToLayer("Heavy"));
+            var numResults = Physics.BoxCastNonAlloc(m_Parent.transform.position, m_Parent.GetComponent<Collider>().bounds.extents,
+                m_Parent.transform.forward, m_RaycastHits, Quaternion.identity, Description.Range, 1 << LayerMask.NameToLayer("Heavy"));
 
             if (numResults == 0 || !m_RaycastHits[0].collider.TryGetComponent(out NetworkObject heavyNetworkObject))
             {
@@ -41,16 +37,6 @@ namespace BossRoom.Server
 
         public override bool Update()
         {
-            /*if (!m_ExecutionFired && (Time.time - TimeStarted) >= Description.ExecTimeSeconds)
-            {
-                m_ExecutionFired = true;
-                var foe = DetectFoe(m_ProvisionalTarget);
-                if (foe != null)
-                {
-                    foe.ReceiveHP(this.m_Parent, -Description.Amount);
-                }
-            }*/
-
             return true;
         }
 
