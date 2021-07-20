43bcc1c840f7ee4cdef26c2bc6da4c396efed54d
Fernando Cortez <fernando.cortez@unity3d.com>
2021-07-19 22:07:32 -0400

    refactoring animator code to client component, self-disabling on server/client components

--- Assets/BossRoom/Scripts/Shared/Client/ClientPickup.cs
+++ Assets/BossRoom/Scripts/Shared/Client/ClientPickup.cs
@@ -1,20 +1,48 @@
 using System;
+using MLAPI;
+using MLAPI.Messaging;
 using UnityEngine;
 
 namespace BossRoom
 {
     [RequireComponent(typeof(ServerPickup))]
-    public class ClientPickup : MonoBehaviour
+    public class ClientPickup : NetworkBehaviour
     {
         [SerializeField]
         ServerPickup m_ServerPickup;
+
+        [SerializeField]
+        Animator m_Animator;
+
+        const string k_PickupBool = "IsCarrying";
+
+        int m_PickupAnimationID;
+
+        void Awake()
+        {
+            m_PickupAnimationID = Animator.StringToHash(k_PickupBool);
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            if (!IsClient || !IsOwner)
+            {
+                enabled = false;
+            }
+        }
 
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.KeypadEnter))
             {
-                m_ServerPickup.Pickup();
+                m_ServerPickup.PickupServerRpc();
             }
+        }
+
+        [ClientRpc]
+        public void SetAnimationTriggerClientRpc(bool isCarrying)
+        {
+            m_Animator.SetBool(m_PickupAnimationID, isCarrying);
         }
     }
 }
--- Assets/BossRoom/Scripts/Shared/Server/ServerPickup.cs
+++ Assets/BossRoom/Scripts/Shared/Server/ServerPickup.cs
@@ -5,34 +5,34 @@ using UnityEngine;
 
 namespace BossRoom
 {
-    public class ServerPickup : MonoBehaviour
+    public class ServerPickup : NetworkBehaviour
     {
+        [SerializeField]
+        ClientPickup m_ClientPickup;
+
         [SerializeField]
         Collider m_Collider;
 
-        [SerializeField]
-        Animator m_Animator;
-
         RaycastHit[] m_RaycastHits = new RaycastHit[1];
-
-        const string k_PickupBool = "IsCarrying";
-        int m_PickupAnimationID;
 
         NetworkObject m_CurrentHeavyObject;
 
-        void Awake()
+        public override void OnNetworkSpawn()
         {
-            m_PickupAnimationID = Animator.StringToHash(k_PickupBool);
+            if (!IsServer)
+            {
+                enabled = false;
+            }
         }
 
         [ServerRpc]
-        public void Pickup()
+        public void PickupServerRpc()
         {
             if (m_CurrentHeavyObject) // ie. drop current item if heavy input has been received
             {
                 m_CurrentHeavyObject.transform.SetParent(null);
                 m_CurrentHeavyObject = null;
-                SetAnimationTriggerClientRpc();
+                m_ClientPickup.SetAnimationTriggerClientRpc(m_CurrentHeavyObject != null);
                 return;
             }
 
@@ -47,14 +47,8 @@ namespace BossRoom
             if (heavyNetworkObject.TrySetParent(transform))
             {
                 m_CurrentHeavyObject = heavyNetworkObject;
-                SetAnimationTriggerClientRpc();
+                m_ClientPickup.SetAnimationTriggerClientRpc(m_CurrentHeavyObject != null);
             }
-        }
-
-        [ClientRpc]
-        void SetAnimationTriggerClientRpc()
-        {
-            m_Animator.SetBool(m_PickupAnimationID, m_CurrentHeavyObject != null);
         }
     }
 }
