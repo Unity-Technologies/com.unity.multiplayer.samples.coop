6dc72a71cc6ad2e5d06168b33faffd866c474e97
Fernando Cortez <fernando.cortez@unity3d.com>
2021-08-18 13:16:10 -0400

    NetworkAnimator added to boss door & floor switch

--- Assets/BossRoom/Scripts/Shared/NetworkDoorState.cs
+++ Assets/BossRoom/Scripts/Shared/NetworkDoorState.cs
@@ -1,12 +1,14 @@
 using MLAPI;
 using MLAPI.NetworkVariable;
-using System.Collections;
 using UnityEngine;
 
-/// <summary>
-/// Network state for a door which can be opened by pressing on a floor switch.
-/// </summary>
-public class NetworkDoorState : NetworkBehaviour
+namespace BossRoom
 {
-    public NetworkVariableBool IsOpen { get; } = new NetworkVariableBool();
+    /// <summary>
+    /// Network state for a door which can be opened by pressing on a floor switch.
+    /// </summary>
+    public class NetworkDoorState : NetworkBehaviour
+    {
+        public NetworkVariableBool IsOpen { get; } = new NetworkVariableBool();
+    }
 }
