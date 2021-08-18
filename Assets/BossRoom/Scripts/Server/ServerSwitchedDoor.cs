9d7a372c0ceb51a71755ee469d535f96abf8872f
Fernando Cortez <fernando.cortez@unity3d.com>
2021-08-18 13:16:10 -0400

    NetworkAnimator added to boss door & floor switch

--- Assets/BossRoom/Scripts/Server/ServerSwitchedDoor.cs
+++ Assets/BossRoom/Scripts/Server/ServerSwitchedDoor.cs
@@ -1,48 +1,72 @@
+using System;
 using MLAPI;
-using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Server-side logic for a door. This particular type of door
-/// is opened when a player stands on a floor switch.
-/// (Assign the floor switches for this door in the editor.)
-/// </summary>
-[RequireComponent(typeof(NetworkDoorState))]
-public class ServerSwitchedDoor : NetworkBehaviour
+namespace BossRoom.Server
 {
-    [SerializeField]
-    public List<NetworkFloorSwitchState> m_SwitchesThatOpenThisDoor;
-
-    private NetworkDoorState m_NetworkDoorState;
-
-    private void Awake()
+    /// <summary>
+    /// Server-side logic for a door. This particular type of door
+    /// is opened when a player stands on a floor switch.
+    /// (Assign the floor switches for this door in the editor.)
+    /// </summary>
+    [RequireComponent(typeof(NetworkDoorState))]
+    public class ServerSwitchedDoor : NetworkBehaviour
     {
-        m_NetworkDoorState = GetComponent<NetworkDoorState>();
+        [SerializeField]
+        NetworkFloorSwitchState[] m_SwitchesThatOpenThisDoor;
 
-        // don't let Update() run until after OnNetworkSpawn()
-        enabled = false;
+        [SerializeField]
+        Animator m_Animator;
 
-        if (m_SwitchesThatOpenThisDoor.Count == 0)
-            Debug.LogError("Door has no switches and can never be opened!", gameObject);
-    }
+        [SerializeField]
+        NetworkDoorState m_NetworkDoorState;
 
-    public override void OnNetworkSpawn()
-    {
-        enabled = IsServer;
-    }
+        const string k_AnimatorDoorOpenBoolVarName = "IsOpen";
 
-    private void Update()
-    {
-        bool isAnySwitchOn = false;
-        foreach (var floorSwitch in m_SwitchesThatOpenThisDoor)
+        [SerializeField, HideInInspector]
+        int m_AnimatorDoorOpenBoolID;
+
+        void Awake()
         {
-            if (floorSwitch && floorSwitch.IsSwitchedOn.Value)
-            {
-                isAnySwitchOn = true;
-                break;
-            }
-        }
-        m_NetworkDoorState.IsOpen.Value = isAnySwitchOn;
-    }
+            // don't let Update() run until after OnNetworkSpawn()
+            enabled = false;
 
+            if (m_SwitchesThatOpenThisDoor.Length == 0)
+                Debug.LogError("Door has no switches and can never be opened!", gameObject);
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            enabled = IsServer;
+
+            DoorStateChanged(false, m_NetworkDoorState.IsOpen.Value);
+
+            m_NetworkDoorState.IsOpen.OnValueChanged += DoorStateChanged;
+        }
+
+        void Update()
+        {
+            var isAnySwitchOn = false;
+            foreach (var floorSwitch in m_SwitchesThatOpenThisDoor)
+            {
+                if (floorSwitch && floorSwitch.IsSwitchedOn.Value)
+                {
+                    isAnySwitchOn = true;
+                    break;
+                }
+            }
+
+            m_NetworkDoorState.IsOpen.Value = isAnySwitchOn;
+        }
+
+        void DoorStateChanged(bool previousValue, bool newValue)
+        {
+            m_Animator.SetBool(m_AnimatorDoorOpenBoolID, newValue);
+        }
+
+        void OnValidate()
+        {
+            m_AnimatorDoorOpenBoolID = Animator.StringToHash(k_AnimatorDoorOpenBoolVarName);
+        }
+    }
 }
