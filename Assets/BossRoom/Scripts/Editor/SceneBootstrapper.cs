7adbfbca2c73bc6f2036794fa39f9f817a75f1d3
Fernando Cortez <fernando.cortez@unity3d.com>
2021-10-19 09:09:25 -0400

    hotfix: scene loading logic race condition fix for globalobjectidhash (#378)
    * scene loading logic race condition fix for globalobjectidhash
    
    * remarks added for the edge case scenario

--- Assets/BossRoom/Scripts/Editor/SceneBootstrapper.cs
+++ Assets/BossRoom/Scripts/Editor/SceneBootstrapper.cs
@@ -6,9 +6,24 @@ namespace Unity.Multiplayer.Samples.BossRoom.Editor
 {
     /// <summary>
     /// Class that permits auto-loading a bootstrap scene when the editor switches play state. This class is
-    /// initialized when Unity loads and when scripts are recompiled. This is to be able to subscribe to
+    /// initialized when Unity is opened and when scripts are recompiled. This is to be able to subscribe to
     /// EditorApplication's playModeStateChanged event, which is when we wish to open a new scene.
     /// </summary>
+    /// <remarks>
+    /// A critical edge case scenario regarding NetworkManager is accounted for here.
+    /// A NetworkObject's GlobalObjectIdHash value is currently generated in OnValidate() which is invoked during a
+    /// build and when the asset is loaded/viewed in the editor.
+    /// If we were to manually open Bootstrap scene via EditorSceneManager.OpenScene(...) as the editor is exiting play
+    /// mode, Bootstrap scene would be entering play mode within the editor prior to having loaded any assets, meaning
+    /// NetworkManager itself has no entry within the AssetDatabase cache. As a result of this, any referenced Network
+    /// Prefabs wouldn't have any entry either.
+    /// To account for this necessary AssetDatabase step, whenever we're redirecting from a new scene, or a scene
+    /// existing in our EditorBuildSettings, we forcefully stop the editor, open Bootstrap scene, and re-enter play
+    /// mode. This provides the editor the chance to create AssetDatabase cache entries for the Network Prefabs assigned
+    /// to the NetworkManager.
+    /// If we are entering play mode directly from Bootstrap scene, no additional steps need to be taken and the scene
+    /// is loaded normally.
+    /// </remarks>
     [InitializeOnLoad]
     public class SceneBootstrapper
     {
@@ -18,6 +33,8 @@ namespace Unity.Multiplayer.Samples.BossRoom.Editor
 
         const string k_LoadBootstrapSceneOnPlay = "Boss Room/Load Bootstrap Scene On Play";
         const string k_DoNotLoadBootstrapSceneOnPlay = "Boss Room/Don't Load Bootstrap Scene On Play";
+
+        static bool s_StoppingAndStarting;
 
         static string BootstrapScene
         {
@@ -87,6 +104,11 @@ namespace Unity.Multiplayer.Samples.BossRoom.Editor
                 return;
             }
 
+            if (s_StoppingAndStarting)
+            {
+                return;
+            }
+
             if (obj == PlayModeStateChange.ExitingEditMode)
             {
                 // cache previous scene so we return to this scene after play session, if possible
@@ -99,8 +121,25 @@ namespace Unity.Multiplayer.Samples.BossRoom.Editor
                     if (!string.IsNullOrEmpty(BootstrapScene) &&
                         System.Array.Exists(EditorBuildSettings.scenes, scene => scene.path == BootstrapScene))
                     {
-                        // scene is included in build settings; open it
-                        EditorSceneManager.OpenScene(BootstrapScene);
+                        var activeScene = EditorSceneManager.GetActiveScene();
+
+                        s_StoppingAndStarting = activeScene.path == string.Empty ||
+                            !BootstrapScene.Contains(activeScene.path);
+
+                        // we only manually inject Bootstrap scene if we are in a blank empty scene,
+                        // or if the active scene is not already BootstrapScene
+                        if (s_StoppingAndStarting)
+                        {
+                            s_StoppingAndStarting = true;
+                            EditorApplication.ExitPlaymode();
+
+                            // scene is included in build settings; open it
+                            EditorSceneManager.OpenScene(BootstrapScene);
+
+                            EditorApplication.EnterPlaymode();
+                            s_StoppingAndStarting = false;
+
+                        }
                     }
                 }
                 else
