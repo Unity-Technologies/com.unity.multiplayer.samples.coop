59c0c66415d176550ab25990d9def09e9e2cc0d8
Samuel Bellomo <samuel.bellomo@unity3d.com>
2021-11-04 14:38:07 -0400

    moving things so they are reusable

---
+++ Packages/com.unity.multiplayer.samples.coop/Utilities/Test.cs
@@ -0,0 +1,18 @@
+using Unity.Netcode;
+
+namespace Unity.Multiplayer.Samples.Utilities
+{
+    public class Test : NetworkBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
