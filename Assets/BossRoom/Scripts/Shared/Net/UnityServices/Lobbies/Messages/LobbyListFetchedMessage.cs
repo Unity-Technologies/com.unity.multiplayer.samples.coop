caa941da72b9fd245ecf0ea7a599536bf690e752
Philipp Deschain <philippd@unity3d.com>
2022-02-23 14:37:55 -0500

    #

--- Assets/BossRoom/Scripts/Shared/Net/UnityServices/Lobbies/Messages/LobbyListFetchedMessage.cs
+++ Assets/BossRoom/Scripts/Shared/Net/UnityServices/Lobbies/Messages/LobbyListFetchedMessage.cs
@@ -5,6 +5,7 @@ namespace Unity.Multiplayer.Samples.BossRoom.Shared.Net.UnityServices.Lobbies
     public struct LobbyListFetchedMessage
     {
         public readonly IReadOnlyList<LocalLobby> LocalLobbies;
+        
         public LobbyListFetchedMessage(List<LocalLobby> localLobbies)
         {
             LocalLobbies = localLobbies;
