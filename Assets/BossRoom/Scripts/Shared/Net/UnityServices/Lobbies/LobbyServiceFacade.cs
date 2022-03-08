2a907da4c291f165024aecd2ae66779edef77e5e
Samuel Bellomo <samuel.bellomo@unity3d.com>
2022-03-07 20:02:17 -0500

    word fix in comment

--- Assets/BossRoom/Scripts/Shared/Net/UnityServices/Lobbies/LobbyServiceFacade.cs
+++ Assets/BossRoom/Scripts/Shared/Net/UnityServices/Lobbies/LobbyServiceFacade.cs
@@ -127,7 +127,7 @@ namespace Unity.Multiplayer.Samples.BossRoom.Shared.Net.UnityServices.Lobbies
                     }
                     m_UnityServiceErrorMessagePub.Publish(new UnityServiceErrorMessage("Host left the lobby","Disconnecting."));
                     ForceLeaveLobbyAttempt();
-                    // no need to disconnect NGO, it should already be handled by NGO callback to disconnect
+                    // no need to disconnect Netcode, it should already be handled by Netcode's callback to disconnect
                 }
             }
         }
