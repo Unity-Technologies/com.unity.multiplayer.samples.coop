0d34e9c816680332a87f5d83ec06ae5727ed6a55
LPLafontaineB <louisphilippe.lb@unity.com>
2022-03-09 17:08:01 -0500

    Revert "Removed guid from ClientPrefs"
    This reverts commit 3d9ebfab1ef8b916bcab98f439c1b4206d21e761.

--- Assets/BossRoom/Scripts/Shared/ClientPrefs.cs
+++ Assets/BossRoom/Scripts/Shared/ClientPrefs.cs
@@ -32,5 +32,23 @@ namespace Unity.Multiplayer.Samples.BossRoom.Client
             PlayerPrefs.SetFloat("MusicVolume", volume);
         }
 
+        /// <summary>
+        /// Either loads a Guid string from Unity preferences, or creates one and checkpoints it, then returns it.
+        /// </summary>
+        /// <returns>The Guid that uniquely identifies this client install, in string form. </returns>
+        public static string GetGuid()
+        {
+            if (PlayerPrefs.HasKey("client_guid"))
+            {
+                return PlayerPrefs.GetString("client_guid");
+            }
+
+            var guid = System.Guid.NewGuid();
+            var guidString = guid.ToString();
+
+            PlayerPrefs.SetString("client_guid", guidString);
+            return guidString;
+        }
+
     }
 }
