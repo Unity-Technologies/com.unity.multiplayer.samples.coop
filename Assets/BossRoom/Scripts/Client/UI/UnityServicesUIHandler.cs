4acbddb42a603815ab45d25a2b823c4dad9bf00d
LPLafontaineB <louisphilippe.lb@unity.com>
2022-03-14 09:40:48 -0400

    feat: replace guid with auth playerid for session management (#488)
    * Replaced custom guid with playerid for session management when signed in through UGS's Authentication Service
    
    * Removed code handling duplicate connections in debug builds
    
    * When not using UGS, player id is now defined as GUID + profile string
    
    (cherry picked from commit 46e2cef6c5aa5691eee3b53ef4a1bf67e0b039a8)

--- Assets/BossRoom/Scripts/Client/UI/UnityServicesUIHandler.cs
+++ Assets/BossRoom/Scripts/Client/UI/UnityServicesUIHandler.cs
@@ -1,5 +1,5 @@
 using System;
-using Unity.Multiplayer.Samples.BossRoom.Client;
+using Unity.Multiplayer.Samples.BossRoom.Shared;
 using Unity.Multiplayer.Samples.BossRoom.Shared.Infrastructure;
 using Unity.Multiplayer.Samples.BossRoom.Shared.Net.UnityServices.Infrastructure;
 using Unity.Services.Lobbies;
@@ -32,7 +32,7 @@ namespace Unity.Multiplayer.Samples.BossRoom.Visual
                     // LobbyConflict can have multiple causes. Let's add other solutions here if there's other situations that arise for this.
                     errorMessage += "\nSee logs for possible causes and solution.";
                     Debug.LogError($"Got service error {error.Message} with LobbyConflict. Possible conflict cause: Trying to play with two builds on the " +
-                                   $"same machine. Please use command line arg '{ClientMainMenuState.AuthProfileCommandLineArg} someName' to set a different auth profile.\n");
+                                   $"same machine. Please use command line arg '{ProfileManager.AuthProfileCommandLineArg} someName' to set a different auth profile.\n");
                 }
             }
 
