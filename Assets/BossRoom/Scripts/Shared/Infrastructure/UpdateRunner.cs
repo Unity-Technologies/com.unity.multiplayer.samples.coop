ec2861c3c543e55fff9ed4a9f84ccc306b3452c3
Philipp Deschain <philippd@unity3d.com>
2022-03-16 13:24:18 -0400

    Added guarding in update runner

--- Assets/BossRoom/Scripts/Shared/Infrastructure/UpdateRunner.cs
+++ Assets/BossRoom/Scripts/Shared/Infrastructure/UpdateRunner.cs
@@ -54,8 +54,11 @@ namespace Unity.Multiplayer.Samples.BossRoom.Shared.Infrastructure
             {
                 m_PendingHandlers.Enqueue(() =>
                 {
-                    m_Subscribers.Add(onUpdate);
-                    m_SubscriberData.Add(onUpdate, new SubscriberData(){Period = period, PeriodCurrent = 0});
+                    if (!m_Subscribers.Contains(onUpdate))
+                    {
+                        m_Subscribers.Add(onUpdate);
+                        m_SubscriberData.Add(onUpdate, new SubscriberData() {Period = period, PeriodCurrent = 0});
+                    }
                 });
             }
         }
