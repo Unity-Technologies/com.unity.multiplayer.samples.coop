a948446f42f6893034ed73d3a08dc310c4ace911
LPLafontaineB <louisphilippe.lb@unity.com>
2022-03-17 15:58:52 -0400

    Adding try/catch to LeaveBeforeQuit to make sure that we quit even if exceptions happen (#563)
    (cherry picked from commit eecfe7ed7411333e7ea0a0776b1f55915aca3276)

--- Assets/BossRoom/Scripts/Shared/ApplicationController.cs
+++ Assets/BossRoom/Scripts/Shared/ApplicationController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using BossRoom.Scripts.Shared.Net.UnityServices.Auth;
 using Unity.Multiplayer.Samples.BossRoom.Client;
@@ -5,6 +6,7 @@ using Unity.Multiplayer.Samples.BossRoom.Server;
 using Unity.Multiplayer.Samples.BossRoom.Shared.Infrastructure;
 using Unity.Multiplayer.Samples.BossRoom.Shared.Net.UnityServices.Infrastructure;
 using Unity.Multiplayer.Samples.BossRoom.Shared.Net.UnityServices.Lobbies;
+using Unity.Multiplayer.Samples.Utilities;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -87,7 +89,15 @@ namespace Unity.Multiplayer.Samples.BossRoom.Shared
         /// </summary>
         private IEnumerator LeaveBeforeQuit()
         {
-            m_LobbyServiceFacade.ForceLeaveLobbyAttempt();
+            // We want to quit anyways, so if anything happens while trying to leave the Lobby, log the exception then carry on
+            try
+            {
+                m_LobbyServiceFacade.ForceLeaveLobbyAttempt();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+            }
             yield return null;
             Application.Quit();
         }
