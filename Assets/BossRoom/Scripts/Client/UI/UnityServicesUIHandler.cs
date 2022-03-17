64ac96609b9a8dcb99958323c38c51d0ededf1b7
LPLafontaineB <louisphilippe.lb@unity.com>
2022-03-17 10:42:28 -0400

    converted if-elses to switch

--- Assets/BossRoom/Scripts/Client/UI/UnityServicesUIHandler.cs
+++ Assets/BossRoom/Scripts/Client/UI/UnityServicesUIHandler.cs
@@ -52,37 +52,33 @@ namespace Unity.Multiplayer.Samples.BossRoom.Visual
             var exception = (LobbyServiceException) error.OriginalException;
             if (exception != null)
             {
-                // If the error is one of the following, the player needs to know about it, so show in a popup message. Otherwise, the log in the console is sufficient.
-                if (exception.Reason == LobbyExceptionReason.LobbyConflict)
+                switch (exception.Reason)
                 {
-                    // LobbyConflict can have multiple causes. Let's add other solutions here if there's other situations that arise for this.
-                    Debug.LogError($"Got service error {error.Message} with LobbyConflict. Possible conflict cause: Trying to play with two builds on the " +
-                        $"same machine. Please use command line arg '{ProfileManager.AuthProfileCommandLineArg} someName' to set a different auth profile.\n");
-                    PopupPanel.ShowPopupPanel("Failed to join Lobby due to a conflict", "See logs for more details.");
-                }
-                else if (exception.Reason == LobbyExceptionReason.LobbyNotFound)
-                {
-                    PopupPanel.ShowPopupPanel("Lobby Not Found", "Requested lobby not found. See logs for details.");
-                }
-                else if (exception.Reason == LobbyExceptionReason.NoOpenLobbies)
-                {
-                    PopupPanel.ShowPopupPanel("Failed to join Lobby", "No accessible lobbies are currently available for quick-join.");
-                }
-                else if (exception.Reason == LobbyExceptionReason.LobbyFull)
-                {
-                    PopupPanel.ShowPopupPanel("Failed to join Lobby", "Lobby is full and can't accept more players.");
-                }
-                else if (exception.Reason == LobbyExceptionReason.Unauthorized)
-                {
-                    PopupPanel.ShowPopupPanel("Lobby error", "Unauthorized.");
-                }
-                else if (exception.Reason == LobbyExceptionReason.RequestTimeOut)
-                {
-                    PopupPanel.ShowPopupPanel("Lobby error", "Request timed out.");
-                }
-                else if (exception.Reason == LobbyExceptionReason.BadRequest)
-                {
-                    PopupPanel.ShowPopupPanel("Lobby error", "Received HTTP error 400 Bad Request from Lobby Service. Is the join code correctly formatted?");
+                    // If the error is one of the following, the player needs to know about it, so show in a popup message. Otherwise, the log in the console is sufficient.
+                    case LobbyExceptionReason.LobbyConflict:
+                        // LobbyConflict can have multiple causes. Let's add other solutions here if there's other situations that arise for this.
+                        Debug.LogError($"Got service error {error.Message} with LobbyConflict. Possible conflict cause: Trying to play with two builds on the " +
+                            $"same machine. Please use command line arg '{ProfileManager.AuthProfileCommandLineArg} someName' to set a different auth profile.\n");
+                        PopupPanel.ShowPopupPanel("Failed to join Lobby due to a conflict", "See logs for more details.");
+                        break;
+                    case LobbyExceptionReason.LobbyNotFound:
+                        PopupPanel.ShowPopupPanel("Lobby Not Found", "Requested lobby not found. See logs for details.");
+                        break;
+                    case LobbyExceptionReason.NoOpenLobbies:
+                        PopupPanel.ShowPopupPanel("Failed to join Lobby", "No accessible lobbies are currently available for quick-join.");
+                        break;
+                    case LobbyExceptionReason.LobbyFull:
+                        PopupPanel.ShowPopupPanel("Failed to join Lobby", "Lobby is full and can't accept more players.");
+                        break;
+                    case LobbyExceptionReason.Unauthorized:
+                        PopupPanel.ShowPopupPanel("Lobby error", "Unauthorized.");
+                        break;
+                    case LobbyExceptionReason.RequestTimeOut:
+                        PopupPanel.ShowPopupPanel("Lobby error", "Request timed out.");
+                        break;
+                    case LobbyExceptionReason.BadRequest:
+                        PopupPanel.ShowPopupPanel("Lobby error", "Received HTTP error 400 Bad Request from Lobby Service. Is the join code correctly formatted?");
+                        break;
                 }
             }
         }
