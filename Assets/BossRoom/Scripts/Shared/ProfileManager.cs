ea8428bcfe2b0ba8f86b8eb7c27392e06b130780
Benoit Doyon <benoit.doyon@unity3d.com>
2022-03-22 09:50:09 -0400

    fix: Generate unique Profile ID on clone project

--- Assets/BossRoom/Scripts/Shared/ProfileManager.cs
+++ Assets/BossRoom/Scripts/Shared/ProfileManager.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
 #if UNITY_EDITOR
+using System;
+using System.Security.Cryptography;
+using System.Text;
 using ParrelSync;
 #endif
 
@@ -31,17 +34,28 @@ namespace Unity.Multiplayer.Samples.BossRoom.Shared
 
                 return hardcodedProfileID;
             }
-#else
-        var arguments = System.Environment.GetCommandLineArgs();
-        for (int i = 0; i < arguments.Length; i++)
-        {
-            if (arguments[i] == AuthProfileCommandLineArg)
-            {
-                var profileId = arguments[i + 1];
-                return profileId;
-            }
-        }
+
+            // When running in the Editor and not a ParrelSync clone, make a unique ID
+            // from the Application.dataPath. This will work for cloning projects
+            // manually, or with Virtual Projects. Since only a single instance of
+            // the Editor can be open for a specific dataPath, uniqueness is ensured.
+            var hashedBytes = new MD5CryptoServiceProvider()
+                .ComputeHash(Encoding.UTF8.GetBytes(Application.dataPath));
+            Array.Resize(ref hashedBytes, 16);
+            return new Guid(hashedBytes).ToString("N");
+
 #endif
+
+            var arguments = System.Environment.GetCommandLineArgs();
+            for (int i = 0; i < arguments.Length; i++)
+            {
+                if (arguments[i] == AuthProfileCommandLineArg)
+                {
+                    var profileId = arguments[i + 1];
+                    return profileId;
+                }
+            }
+
             return "";
         }
     }
