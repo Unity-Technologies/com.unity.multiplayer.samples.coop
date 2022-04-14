709ebccd23ae1da0fa820f30b1ed6b6d2192379b
Samuel Bellomo <samuel.bellomo@unity3d.com>
2022-04-14 19:28:32 -0400

    cleanup

--- RepoUtilities/InstallHooks.sh
+++ RepoUtilities/InstallHooks.sh
@@ -1,7 +1,6 @@
 echo "starting"
 cp pre-commit.toInstall ../.git/hooks/pre-commit
 chmod +x ../.git/hooks/pre-commit
-# python3 is necessary to run these hooks, so testing if python is installed
 echo "checking if python is installed, if the following errors out, please install python3"
 
 python3 -c "print('done installing')"
