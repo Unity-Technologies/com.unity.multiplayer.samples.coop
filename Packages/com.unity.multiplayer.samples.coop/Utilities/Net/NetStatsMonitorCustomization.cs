52f45c4534e28e6834706fd0d3837a3147fda6e5
Samuel Bellomo <samuel.bellomo@unity3d.com>
2022-04-14 17:36:05 -0400

    adding mobile support

--- Packages/com.unity.multiplayer.samples.coop/Utilities/Net/NetStatsMonitorCustomization.cs
+++ Packages/com.unity.multiplayer.samples.coop/Utilities/Net/NetStatsMonitorCustomization.cs
@@ -6,6 +6,8 @@ public class NetStatsMonitorCustomization : MonoBehaviour
     [SerializeField]
     RuntimeNetStatsMonitor m_Monitor;
 
+    const int k_NbTouchesToOpenWindow = 3;
+
     void Start()
     {
         m_Monitor.enabled = false;
@@ -13,9 +15,22 @@ public class NetStatsMonitorCustomization : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.S))
+        if (Input.GetKeyUp(KeyCode.S) || Input.touchCount == k_NbTouchesToOpenWindow && AnyTouchDown())
         {
             m_Monitor.enabled = !m_Monitor.enabled; // toggle
         }
+    }
+
+    static bool AnyTouchDown()
+    {
+        foreach (var touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
