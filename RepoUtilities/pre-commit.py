2ae194a661ca95760710d45b808a08ca3ae44abb
Samuel Bellomo <samuel.bellomo@unity3d.com>
2022-04-14 19:19:46 -0400

    adding git lfs support

--- RepoUtilities/pre-commit.py
+++ RepoUtilities/pre-commit.py
@@ -12,14 +12,19 @@ def GetStagedFileContent(filePath):
     stream = os.popen(cmd)
     return stream.read()
 
-output = content = GetStagedFileContent('Packages/manifest.json')
+def GetLFSStagedFileContent(filePath):
+    cmd = f"git cat-file blob :{filePath} | git lfs smudge"
+    stream = os.popen(cmd)
+    return stream.read()
+
+output = GetStagedFileContent('Packages/manifest.json')
 packages = json.loads(output)['dependencies']
 if ('com.unity.multiplayer.virtualprojects' in packages):
     FailCommit("Virtual projects in packages")
 if ('github' in packages['com.unity.multiplayer.tools'].lower()):
     FailCommit("Tools using github package")
 
-content = GetStagedFileContent('ProjectSettings/ProjectSettings.asset')
+content = GetLFSStagedFileContent('ProjectSettings/ProjectSettings.asset')
 res = re.search(r'.*cloudProjectId:.*\w+\s*\n', content) != None
 if res:
     FailCommit("Detected cloud project id in project settings")
