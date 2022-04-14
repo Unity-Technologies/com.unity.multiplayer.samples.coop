f00620c10b448d1eef0b0b2ea3769ddbea78eb2f
Philipp Deschain <pdeschain@users.noreply.github.com>
2022-04-14 10:36:25 -0400

    Update Assets/BossRoom/Scripts/Client/Game/State/ClientMainMenuState.cs
    Co-authored-by: Fernando Cortez <fernando.cortez@unity3d.com>

--- Assets/BossRoom/Scripts/Client/Game/State/ClientMainMenuState.cs
+++ Assets/BossRoom/Scripts/Client/Game/State/ClientMainMenuState.cs
@@ -47,7 +47,7 @@ namespace Unity.Multiplayer.Samples.BossRoom.Client
         {
             if (string.IsNullOrEmpty(Application.cloudProjectId))
             {
-                PopupManager.ShowPopupPanel("Unity Game Services ProjectID not set up" ,"Click the Readme file in the Assets Folder within the Project window in-editor to follow \"Setting Up Unity Game Services\"");
+                PopupManager.ShowPopupPanel("Unity Game Services ProjectID not set up" ,"Click the Readme file in the Assets Folder within the Project window in-editor to follow \"How to set up Unity Game Services\"");
                 OnSignInFailed();
                 return;
             }
