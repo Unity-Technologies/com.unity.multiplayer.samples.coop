2803c38c8812c63e5343af4214809d6785716a92
Samuel Bellomo <samuel.bellomo@unity3d.com>
2022-05-04 12:18:19 -0400

    Adding instructions for RNSM

---
+++ Packages/com.unity.multiplayer.samples.coop/Utilities/Net/RNSM/AutoHide.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+
+public class AutoHide : MonoBehaviour
+{
+    [SerializeField]
+    float m_TimeToHideSeconds = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(HideAfterSeconds());
+    }
+
+    IEnumerator HideAfterSeconds()
+    {
+        yield return new WaitForSeconds(m_TimeToHideSeconds);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Packages/com.unity.multiplayer.samples.coop/Utilities/Net/RNSM/NetStatsMonitorCustomization.cs
@@ -0,0 +1,39 @@
+using Unity.Multiplayer.Tools.NetStatsMonitor;
+using UnityEngine;
+
+namespace Unity.Multiplayer.Samples.Utilities.ClientAuthority
+{
+    public class NetStatsMonitorCustomization : MonoBehaviour
+    {
+        [SerializeField]
+        RuntimeNetStatsMonitor m_Monitor;
+
+        const int k_NbTouchesToOpenWindow = 3;
+
+        void Start()
+        {
+            m_Monitor.enabled = false;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.S) || Input.touchCount == k_NbTouchesToOpenWindow && AnyTouchDown())
+            {
+                m_Monitor.enabled = !m_Monitor.enabled; // toggle
+            }
+        }
+
+        static bool AnyTouchDown()
+        {
+            foreach (var touch in Input.touches)
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
