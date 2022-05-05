af06929c8899fc2770680b1a1a60b24438ba449f
LPLafontaineB <louisphilippe.lb@unity.com>
2022-05-05 15:51:03 -0400

    extracting playerprefs keys as constants

--- Assets/BossRoom/Scripts/Shared/ClientPrefs.cs
+++ Assets/BossRoom/Scripts/Shared/ClientPrefs.cs
@@ -9,27 +9,32 @@ namespace Unity.Multiplayer.Samples.BossRoom.Client
     /// </summary>
     public static class ClientPrefs
     {
+        const string k_MasterVolumeKey = "MasterVolume";
+        const string k_MusicVolumeKey = "MusicVolume";
+        const string k_ClientGUIDKey = "client_guid";
+        const string k_AvailableProfilesKey = "AvailableProfiles";
+
         const float k_DefaultMasterVolume = 0.5f;
         const float k_DefaultMusicVolume = 0.8f;
 
         public static float GetMasterVolume()
         {
-            return PlayerPrefs.GetFloat("MasterVolume", k_DefaultMasterVolume);
+            return PlayerPrefs.GetFloat(k_MasterVolumeKey, k_DefaultMasterVolume);
         }
 
         public static void SetMasterVolume(float volume)
         {
-            PlayerPrefs.SetFloat("MasterVolume", volume);
+            PlayerPrefs.SetFloat(k_MasterVolumeKey, volume);
         }
 
         public static float GetMusicVolume()
         {
-            return PlayerPrefs.GetFloat("MusicVolume", k_DefaultMusicVolume);
+            return PlayerPrefs.GetFloat(k_MusicVolumeKey, k_DefaultMusicVolume);
         }
 
         public static void SetMusicVolume(float volume)
         {
-            PlayerPrefs.SetFloat("MusicVolume", volume);
+            PlayerPrefs.SetFloat(k_MusicVolumeKey, volume);
         }
 
         /// <summary>
@@ -38,26 +43,26 @@ namespace Unity.Multiplayer.Samples.BossRoom.Client
         /// <returns>The Guid that uniquely identifies this client install, in string form. </returns>
         public static string GetGuid()
         {
-            if (PlayerPrefs.HasKey("client_guid"))
+            if (PlayerPrefs.HasKey(k_ClientGUIDKey))
             {
-                return PlayerPrefs.GetString("client_guid");
+                return PlayerPrefs.GetString(k_ClientGUIDKey);
             }
 
             var guid = System.Guid.NewGuid();
             var guidString = guid.ToString();
 
-            PlayerPrefs.SetString("client_guid", guidString);
+            PlayerPrefs.SetString(k_ClientGUIDKey, guidString);
             return guidString;
         }
 
         public static string GetAvailableProfiles()
         {
-            return PlayerPrefs.GetString("available_profiles", "");
+            return PlayerPrefs.GetString(k_AvailableProfilesKey, "");
         }
 
         public static void SetAvailableProfiles(string availableProfiles)
         {
-            PlayerPrefs.SetString("available_profiles", availableProfiles);
+            PlayerPrefs.SetString(k_AvailableProfilesKey, availableProfiles);
         }
 
     }
