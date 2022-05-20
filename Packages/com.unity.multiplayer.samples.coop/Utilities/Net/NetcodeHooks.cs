795f89997edabe2cb33ec0035bbf31bf0f413594
Samuel Bellomo <samuel.bellomo@unity3d.com>
2022-05-20 18:12:38 -0400

    Initial pass at solving issue with buffer overlfow and weird issues. these stripped scripts don’t contain netcode data or RPCs, but they are still NetworkBehaviours (since they want to have access to a OnNetworkSpawn). Which messes with the NetworkBehaviour index in NetworkObject. Currently looking at why GetComponentsInChildren will return null scripts when looking for MonoBehaviours, but not for NetworkBehaviours (component count was different client side and server side)
    Removing NetworkBehaviours in client script so we don't have these index offsets. Adding a NetcodeHooks script to get access to OnNetworkSpawn from non-NetworkBehaviour scripts. (Can't use scene load events as these characters could spawn dynamically as well).
    This commit fixes this for imps, need to fix this for other characters as well.

---
+++ Packages/com.unity.multiplayer.samples.coop/Utilities/Net/NetcodeHooks.cs
@@ -0,0 +1,27 @@
+using Unity.Netcode;
+using System;
+
+
+namespace Unity.Multiplayer.Samples.Utilities
+{
+    // useful for classes that can't be NetworkBehaviours themselves (for example, with dedicated servers, you can't have a NetworkBehaviour that exists
+    // on clients but gets stripped on the server, this will mess with your NetworkBehaviour indexing.
+    public class NetcodeHooks : NetworkBehaviour
+    {
+        public event Action OnNetworkSpawnHook;
+
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+            OnNetworkSpawnHook?.Invoke();
+        }
+
+        public event Action OnNetworkDespawnHook;
+
+        public override void OnNetworkDespawn()
+        {
+            base.OnNetworkDespawn();
+            OnNetworkDespawnHook?.Invoke();
+        }
+    }
+}
