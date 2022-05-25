46d5a9a3efb48711407c986608fc1204109b92b8
Samuel Bellomo <samuel.bellomo@unity3d.com>
2022-05-25 16:40:01 -0400

    removing dead code that messed with scene states that were split between two scenes.

--- Assets/BossRoom/Scripts/Shared/Game/State/GameStateBehaviour.cs
+++ Assets/BossRoom/Scripts/Shared/Game/State/GameStateBehaviour.cs
@@ -39,14 +39,6 @@ namespace Unity.Multiplayer.Samples.BossRoom
     public abstract class GameStateBehaviour : MonoBehaviour
     {
         /// <summary>
-        /// Does this GameState persist across multiple scenes?
-        /// </summary>
-        public virtual bool Persists
-        {
-            get { return false; }
-        }
-
-        /// <summary>
         /// What GameState this represents. Server and client specializations of a state should always return the same enum.
         /// </summary>
         public abstract GameState ActiveState { get; }
@@ -88,46 +80,26 @@ namespace Unity.Multiplayer.Samples.BossRoom
         {
             if (s_ActiveStateGO != null)
             {
-                if (s_ActiveStateGO == gameObject)
+                if (s_ActiveStateGO == gameObject || s_ActiveStateGO.GetComponent<GameStateBehaviour>().ActiveState == ActiveState)
                 {
                     //nothing to do here, if we're already the active state object.
                     return;
                 }
 
-                //on the host, this might return either the client or server version, but it doesn't matter which;
-                //we are only curious about its type, and its persist state.
-                var previousState = s_ActiveStateGO.GetComponent<GameStateBehaviour>();
-
-                if (previousState.Persists && previousState.ActiveState == ActiveState)
-                {
-                    //we need to make way for the DontDestroyOnLoad state that already exists.
-                    Destroy(gameObject);
-                    return;
-                }
-
-                //otherwise, the old state is going away. Either it wasn't a Persisting state, or it was,
+                //the old state is going away. Either it wasn't a Persisting state, or it was,
                 //but we're a different kind of state. In either case, we're going to be replacing it.
                 Destroy(s_ActiveStateGO);
             }
 
             s_ActiveStateGO = gameObject;
-            if (Persists)
-            {
-                DontDestroyOnLoad(gameObject);
-            }
         }
 
-        protected virtual void InitializeScope()
-        {
-        }
+        protected virtual void InitializeScope() { }
 
         public virtual void OnDestroy()
         {
             Scope?.Dispose();
-            if (!Persists)
-            {
-                s_ActiveStateGO = null;
-            }
+            s_ActiveStateGO = null;
         }
     }
 }
