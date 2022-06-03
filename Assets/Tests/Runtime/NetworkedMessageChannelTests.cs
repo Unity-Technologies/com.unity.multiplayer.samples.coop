0b730585cb2d7b75d71c478a5e2fdf16b71e93fa
LPLafontaineB <louisphilippe.lb@unity.com>
2022-06-03 13:41:30 -0400

    adding empty test

--- Assets/Tests/Runtime/NetworkedMessageChannelTests.cs
+++ Assets/Tests/Runtime/NetworkedMessageChannelTests.cs
@@ -1,10 +1,34 @@
+using System.Collections;
+using Unity.Collections;
+using Unity.Multiplayer.Samples.BossRoom.Shared.Infrastructure;
+using Unity.Netcode;
 using UnityEngine;
 using Unity.Netcode.TestHelpers.Runtime;
+using UnityEngine.Assertions;
+using UnityEngine.TestTools;
 
 namespace Unity.Multiplayer.Samples.BossRoom.Tests.Runtime
 {
     public class NetworkedMessageChannelTests: NetcodeIntegrationTest
     {
+        struct EmptyMessage : INetworkSerializeByMemcpy { }
+
+        struct GenericMessage : INetworkSerializeByMemcpy
+        {
+        }
+
         protected override int NumberOfClients => 2;
+
+        NetworkManager FirstClient => m_ClientNetworkManagers[0];
+        NetworkManager SecondClient => m_ClientNetworkManagers[1];
+
+        [UnityTest]
+        public IEnumerator NetworkedMessageIsReceivedByAllSubscribersOnSingleClient()
+        {
+            var emptyMessageChannel = new NetworkedMessageChannel<EmptyMessage>(FirstClient);
+
+            yield return null;
+
+        }
     }
 }
