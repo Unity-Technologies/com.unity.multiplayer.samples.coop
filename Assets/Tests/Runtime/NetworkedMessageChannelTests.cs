70244a27d63a1df4359e4a38b5df0402dfdd22c7
LPLafontaineB <louisphilippe.lb@unity.com>
2022-06-03 16:22:27 -0400

    Adding test to validate messages are being received properly
    * should probably split this into multiple smaller tests

--- Assets/Tests/Runtime/NetworkedMessageChannelTests.cs
+++ Assets/Tests/Runtime/NetworkedMessageChannelTests.cs
@@ -15,6 +15,7 @@ namespace Unity.Multiplayer.Samples.BossRoom.Tests.Runtime
 
         struct GenericMessage : INetworkSerializeByMemcpy
         {
+            public bool value;
         }
 
         protected override int NumberOfClients => 2;
@@ -25,9 +26,43 @@ namespace Unity.Multiplayer.Samples.BossRoom.Tests.Runtime
         [UnityTest]
         public IEnumerator NetworkedMessageIsReceivedByAllSubscribersOnSingleClient()
         {
-            var emptyMessageChannel = new NetworkedMessageChannel<EmptyMessage>(FirstClient);
+            var emptyMessageChannelClient = new NetworkedMessageChannel<EmptyMessage>(FirstClient);
+            var emptyMessageChannelServer = new NetworkedMessageChannel<EmptyMessage>(m_ServerNetworkManager);
+
+            var genericMessageChannelClient = new NetworkedMessageChannel<GenericMessage>(FirstClient);
+            var genericMessageChannelServer = new NetworkedMessageChannel<GenericMessage>(m_ServerNetworkManager);
+
+            var subscriptions = new DisposableGroup();
+            var nbMessagesReceived = 0;
+            subscriptions.Add(emptyMessageChannelClient.Subscribe(OnEmptyMessageReceived));
+            subscriptions.Add(emptyMessageChannelClient.Subscribe(OnEmptyMessageReceived2));
+            subscriptions.Add(genericMessageChannelClient.Subscribe(OnGenericMessageReceived));
+
+            void OnEmptyMessageReceived(EmptyMessage message)
+            {
+                nbMessagesReceived++;
+            }
+
+            void OnEmptyMessageReceived2(EmptyMessage message)
+            {
+                nbMessagesReceived++;
+            }
+
+            void OnGenericMessageReceived(GenericMessage message)
+            {
+                nbMessagesReceived++;
+                Assert.IsTrue(message.value);
+            }
 
             yield return null;
+
+            emptyMessageChannelServer.Publish(new EmptyMessage());
+            emptyMessageChannelServer.Publish(new EmptyMessage());
+            genericMessageChannelServer.Publish(new GenericMessage() {value = true});
+
+            yield return new WaitForSeconds(1);
+
+            Assert.AreEqual(5, nbMessagesReceived);
 
         }
     }
