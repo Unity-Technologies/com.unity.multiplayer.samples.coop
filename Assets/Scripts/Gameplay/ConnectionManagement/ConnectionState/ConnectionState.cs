02a4fe4006095af00db2a3c93fb5dbff851c52ec
LPLafontaineB <louisphilippe.lb@unity.com>
2022-06-21 16:02:15 -0400

    moving method description to class that actually implements it

--- Assets/Scripts/Gameplay/ConnectionManagement/ConnectionState/ConnectionState.cs
+++ Assets/Scripts/Gameplay/ConnectionManagement/ConnectionState/ConnectionState.cs
@@ -56,19 +56,6 @@ namespace Unity.Multiplayer.Samples.BossRoom
 
         public virtual void OnDisconnectReasonReceived(ConnectStatus disconnectReason) { }
 
-        /// <summary>
-        /// This logic plugs into the "ConnectionApprovalCallback" exposed by the NetworkManager, and is run every time a client connects to us.
-        /// </summary>
-        /// <remarks>
-        /// Since our game doesn't have to interact with some third party authentication service to validate the identity of the new connection, our ApprovalCheck
-        /// method is simple, and runs synchronously, invoking "callback" to signal approval at the end of the method. Netcode currently doesn't support the ability
-        /// to send back more than a "true/false", which means we have to work a little harder to provide a useful error return to the client. To do that, we invoke a
-        /// custom message in the same channel that Netcode uses for its connection callback. Since the delivery is NetworkDelivery.ReliableSequenced, we can be
-        /// confident that our login result message will execute before any disconnect message.
-        /// </remarks>
-        /// <param name="connectionData">binary data passed into StartClient. In our case this is defined by the class ConnectionPayload. </param>
-        /// <param name="clientId">This is the clientId that Netcode assigned us on login. It does not persist across multiple logins from the same client. </param>
-        /// <param name="connectionApprovedCallback">The delegate we must invoke to signal that the connection was approved or not. </param>
         public virtual void ApprovalCheck(byte[] connectionData, ulong clientId, NetworkManager.ConnectionApprovedDelegate connectionApprovedCallback) { }
     }
 }
