6047a2d1089606aa06d43b0db1fe0bff3bdebcc4
Philipp Deschain <pdeschain@users.noreply.github.com>
2022-09-21 16:30:14 -0400

    Update Assets/Scripts/Gameplay/GameplayObjects/Character/ServerCharacter.cs
    Co-authored-by: Sam Bellomo <71790295+SamuelBellomo@users.noreply.github.com>

--- Assets/Scripts/Gameplay/GameplayObjects/Character/ServerCharacter.cs
+++ Assets/Scripts/Gameplay/GameplayObjects/Character/ServerCharacter.cs
@@ -12,6 +12,7 @@ namespace Unity.BossRoom.Gameplay.GameplayObjects.Character
 {
     /// <summary>
     /// Contains all NetworkVariables, RPCs and server-side logic of a character.
+    /// This class was separated in two to keep client and server context self contained. This way you don't have to continuously ask yourself if code is running client or server side.
     /// </summary>
     [RequireComponent(typeof(NetworkHealthState),
         typeof(NetworkLifeState),
