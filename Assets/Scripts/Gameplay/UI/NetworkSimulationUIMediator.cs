a8edcab1e4909ef2f47594a27766d3f168d7a8b0
LPLafontaineB <louisphilippe.lb@unity.com>
2023-05-08 16:47:27 -0400

    Adding lag spike and disconnect simulation
    (cherry picked from commit 4b9c7c6698a64caa8926e54f81dc16a2160d3d67)

--- Assets/Scripts/Gameplay/UI/NetworkSimulationUIMediator.cs
+++ Assets/Scripts/Gameplay/UI/NetworkSimulationUIMediator.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using TMPro;
 using Unity.Multiplayer.Tools.NetworkSimulator.Runtime;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Unity.BossRoom.Gameplay.UI
 {
@@ -19,13 +19,18 @@ namespace Unity.BossRoom.Gameplay.UI
         TMP_Dropdown m_Dropdown;
 
         [SerializeField]
+        TMP_InputField m_LagSpikeDuration;
+
+        [SerializeField]
         KeyCode m_OpenWindowKeyCode = KeyCode.Tilde;
 
         const int k_NbTouchesToOpenWindow = 5;
 
         Dictionary<string, INetworkSimulatorPreset> m_SimulatorPresets = new Dictionary<string, INetworkSimulatorPreset>();
 
-        void Start()
+        bool m_Shown;
+
+        void Awake()
         {
             var optionData = new List<TMP_Dropdown.OptionData>();
             foreach (var networkSimulatorPreset in NetworkSimulatorPresets.Values)
@@ -35,6 +40,7 @@ namespace Unity.BossRoom.Gameplay.UI
             }
             m_Dropdown.AddOptions(optionData);
             m_Dropdown.onValueChanged.AddListener(OnPresetChanged);
+            Hide();
         }
 
         void OnPresetChanged(int optionIndex)
@@ -45,17 +51,23 @@ namespace Unity.BossRoom.Gameplay.UI
 
         public void Hide()
         {
-            m_CanvasGroup.alpha = 0;
+            m_CanvasGroup.alpha = 0f;
+            m_CanvasGroup.interactable = false;
+            m_CanvasGroup.blocksRaycasts = false;
+            m_Shown = false;
         }
 
         void Show()
         {
-            m_CanvasGroup.alpha = 1;
+            m_CanvasGroup.alpha = 1f;
+            m_CanvasGroup.interactable = true;
+            m_CanvasGroup.blocksRaycasts = true;
+            m_Shown = true;
         }
 
         void ToggleVisibility()
         {
-            if (m_CanvasGroup.alpha > 0)
+            if (m_Shown)
             {
                 Hide();
             }
@@ -67,10 +79,20 @@ namespace Unity.BossRoom.Gameplay.UI
 
         void Update()
         {
-            if (Input.touchCount == k_NbTouchesToOpenWindow && AnyTouchDown() ||
-                m_OpenWindowKeyCode != KeyCode.None && Input.GetKeyDown(m_OpenWindowKeyCode))
+            if (m_NetworkSimulator.IsAvailable)
             {
-                ToggleVisibility();
+                if (Input.touchCount == k_NbTouchesToOpenWindow && AnyTouchDown() ||
+                    m_OpenWindowKeyCode != KeyCode.None && Input.GetKeyDown(m_OpenWindowKeyCode))
+                {
+                    ToggleVisibility();
+                }
+            }
+            else
+            {
+                if (m_Shown)
+                {
+                    Hide();
+                }
             }
         }
 
@@ -84,6 +106,22 @@ namespace Unity.BossRoom.Gameplay.UI
                 }
             }
             return false;
+        }
+
+        public void SimulateDisconnect()
+        {
+            m_NetworkSimulator.Disconnect();
+        }
+
+        public void TriggerLagSpike()
+        {
+            double.TryParse(m_LagSpikeDuration.text, out var duration);
+            m_NetworkSimulator.TriggerLagSpike(TimeSpan.FromMilliseconds(duration));
+        }
+
+        public void SanitizeLagSpikeDurationInputField()
+        {
+            m_LagSpikeDuration.text = Regex.Replace(m_LagSpikeDuration.text, "[^0-9]", "");
         }
     }
 }
