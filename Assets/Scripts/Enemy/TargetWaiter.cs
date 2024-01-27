3ef30e0ad28e58624c1abba0cefd669b4ba4b7a3
khjj07 <khjj07@gmail.com>
2024-01-28 06:17:17 +0900

    lfs 문제

--- Assets/Scripts/Enemy/TargetWaiter.cs
+++ Assets/Scripts/Enemy/TargetWaiter.cs
@@ -1,21 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace PanicBuying
-{
-    public class TargetWaiter : TargetFinder
-    {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:64da5c77082f681f6e02c2a2ca5658deea024b7dd017b408980a6d71a91bd670
+size 365
