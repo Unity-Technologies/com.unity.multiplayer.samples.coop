969aa2225f57798abcfb8903961d9748172cc059
Kálmán Bálint <kalman.balint@unity3d.com>
2025-02-25 17:28:48 -0500

    cleanup

--- Assets/Scripts/Gameplay/UI/MessageFeed.cs
+++ Assets/Scripts/Gameplay/UI/MessageFeed.cs
@@ -16,6 +16,7 @@ public class MessageFeed : MonoBehaviour
     UIDocument doc;
 
     List<MessageViewModel> m_Messages;
+    List<MessageViewModel> _messagesToRemove = new List<MessageViewModel>();
 
     ListView m_MessageContainer;
 
@@ -102,6 +103,7 @@ public class MessageFeed : MonoBehaviour
         ShowMessage($"Hello! {DateTime.Now.Millisecond}");
     }
 
+    //would be much nicer if this would be a custom control, and we'd do this in an attach to panel event
     void Start()
     {
         var root = doc.rootVisualElement;
@@ -118,16 +120,11 @@ public class MessageFeed : MonoBehaviour
             var newBox = new VisualElement();
             newBox.AddToClassList("messageBox");
 
-            //newBox.style.position = Position.Absolute; // Explicitly position it
-
-            // Position the new message box below the last message
-            //newBox.style.top = m_MessageContainer.childCount * (messageHeight + verticalSpacing);
-
             var newLabel = new Label();
             newLabel.AddToClassList("message");
             newBox.Add(newLabel);
 
-            // the even when the control get's added to the "UI Canvas"
+            // the event when the control get's added to the "UI Canvas"
             newBox.RegisterCallback<AttachToPanelEvent>((e) =>
             {
                 if (e.target is VisualElement element)
@@ -137,7 +134,7 @@ public class MessageFeed : MonoBehaviour
                 }
             });
 
-            // fires before the element is actually removed
+            // fires just before the element is actually removed
             newBox.RegisterCallback<DetachFromPanelEvent>((e) =>
             {
                 if (e.target is VisualElement) { }
@@ -182,7 +179,6 @@ public class MessageFeed : MonoBehaviour
         }
     }
 
-    List<MessageViewModel> _messagesToRemove = new List<MessageViewModel>();
 
     void Update()
     {
@@ -219,21 +215,6 @@ public class MessageFeed : MonoBehaviour
 
     void ShowMessage(string message)
     {
-        // Reuse or create a new message
-        // MessageViewModel newMessage = null;
-
-        // a list view's virtualization logic actually takes care of this by default
-        /*
-        foreach (var m in m_Messages)
-        {
-            if (!m.isShown)
-            {
-                // Reuse the hidden message
-                newMessage = m;
-                break;
-            }
-        }
-        */
         var newMessage = new MessageViewModel(message, TimeSpan.FromSeconds(5));
 
         m_Messages.Add(newMessage); // Add to the list of messages
@@ -246,22 +227,6 @@ public class MessageFeed : MonoBehaviour
         element.ToggleInClassList(className);
     }
 
-    
-    static void StartFadeout(VisualElement message, float opacity)
-    {
-        message.schedule.Execute(() =>
-        {
-            opacity -= 0.01f;
-            message.style.opacity = opacity;
-
-            if (opacity <= 0)
-            {
-                // Once faded out fully, hide the message and reset state
-                message.style.display = DisplayStyle.None;
-            }
-        }).Every((long)0.1f).Until(() => opacity <= 0);
-    }
-    
 
     // if you bind the itemsource to the list you don't actually have to manually do this
     private class MessageViewModel
